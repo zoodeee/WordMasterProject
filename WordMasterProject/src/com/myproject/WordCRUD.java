422d4cdac01ff5bd4df87dc25e73a08c8752bc85
JuYong <22100181@handong.ac.kr>
2022-09-06 23:00:03 +0900

    Add Word

--- WordMasterProject/src/com/myproject/WordCRUD.java
+++ WordMasterProject/src/com/myproject/WordCRUD.java
@@ -1,13 +1,35 @@
 package com.myproject;
 
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+	ArrayList<Word> list;
+	Scanner s;
+	
+	WordCRUD(Scanner s){
+		list=new ArrayList<>();
+		this.s=s;
+	}
+	
 	@Override
 	public Object add() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+		int level=s.nextInt();
+		String word=s.nextLine();
+		
+		System.out.print("뜻 입력 : ");
+		String meaning=s.nextLine();
+		
+		return new Word(0, level, word, meaning);
 	}
 
+	public void addWord() {
+		Word one=(Word)add();
+		list.add(one);
+		System.out.println("새 단어가 단어장에 추가되었습니다 !!!\n");
+	}
+	
 	@Override
 	public int update(Object obj) {
 		// TODO Auto-generated method stub
--- WordMasterProject/src/com/myproject/WordManager.java
+++ WordMasterProject/src/com/myproject/WordManager.java
@@ -1,22 +1,15 @@
 package com.myproject;
 
+import java.util.Scanner;
+
 public class WordManager {
-	/*
-	 *** 영단어 마스터 ***
-	 ********************
-	1. 모든 단어 보기
-	2. 수준별 단어 보기
-	3. 단어 검색
-	4. 단어 추가
-	5. 단어 수정
-	6. 단어 삭제
-	7. 파일 저장
-	0. 나가기
-	 ********************
-	=> 원하는 메뉴는?
-	 */
+	Scanner s=new Scanner(System.in);
+	WordCRUD wordCRUD;
 	
-	public void selectMenu() {
+	WordManager(){ 
+		wordCRUD=new WordCRUD(s);
+	}
+	public int selectMenu() {
 		System.out.print("*** 영단어 마스터 ***\n"
 				+ "********************\n"
 				+ "1. 모든 단어 보기\n"
@@ -29,9 +22,21 @@ public class WordManager {
 				+ "0. 나가기\n"
 				+ "********************\n"
 				+ "=> 원하는 메뉴는? ");
+		return s.nextInt();
 	}
 	
 	public void start() {
-		selectMenu();
+		while(true) {
+			int menu=selectMenu();
+			if(menu==0) break;
+			if(menu==4) {
+				wordCRUD.addWord();
+			}
+			else if(menu==1) {
+				//list
+			}
+		}
+		
+		
 	}
 }
